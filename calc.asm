37e146134785beb8d880e699b622c29c4c78bf92
Piotr Szczygieł <szczygiel.p@gmail.com>
2019-03-05 08:52:51 +0100

    Change code formatting

--- calc.asm
+++ calc.asm
@@ -1,69 +1,56 @@
 ; Piotr Szczygieł - Assemblery 2019
 ; Simple calculator
+format MZ                                                   ; DOS MZ executable
+stack 256                                                   ; set stack size to 256 bytes
+entry main:start                                            ; specify application entry point
 
-; DOS MZ executable
-format MZ
-
-; specify application entry point
-entry main:start
-
-; set stack size to 256 bytes
-stack 256
-
-; main code segment
-segment main
-
-    ; application entry point
-    start:
-        ; initialize the data segment
-        mov ax, word text
+segment main                                                ; main code segment
+    start:                                                  ; application entry point
+        mov ax, word text                                   ; initialize the data segment
         mov ds, ax
 
-        ; display the welcome string
-        mov dx, word string_welcome
-        call print
+        mov dx, word string_welcome                         ; display the welcome string
+        call print_string
+        mov dx, word string_type_exit                       ; display 'type exit to terminate'
+        call print_string
 
-        main_loop:
-            ; display the prompt
+        sub esp, 12                                         ; allocate space on stack for 6 variables
+        main_loop:                                          ; main loop entry
+            mov dx, word string_newline                     ; display prompt
+            call print_string
             mov dx, word string_prompt
-            call print
+            call print_string
 
-            ; read user input into string_input buffer
-            call read
-            mov dx, word string_newline
-            call print
+            call read_string_input                          ; read user keyboard input into string_input
 
-            ; reserve space for 6 variables
-            sub esp, 12
+            mov dx, word string_newline                     ; move to next line
+            call print_string
 
-            ; check if first word exists
             mov bx, word string_input + 1
-            mov [esp + 2], bx       ; first word address
-            call get_word_length
+            mov [esp + 2], bx                               ; first word address
+            call get_word_length                            ; check if first word exists
             cmp ax, 0
             je invalid_input
-            mov [esp + 4], ax       ; first word length
+            mov [esp + 4], ax                               ; first word length
 
-            ; check if second word exists
-            inc ax                  ; skip the space
+            inc ax                                          ; skip the space
             mov bx, word string_input + 1
             add bx, ax
-            mov [esp + 6], bx       ; second word address
-            call get_word_length
+            mov [esp + 6], bx                               ; second word address
+            call get_word_length                            ; check if second word exists
             cmp ax, 0
             je invalid_input
-            mov [esp + 8], ax       ; second word length
+            mov [esp + 8], ax                               ; second word length
 
-            ; check if third word exists
             mov bx, word string_input + 1
-            add bx, [esp + 4]       ; skip to the third word by adding to base
-            add bx, [esp + 8]       ; address combined length of two previous words
-            add bx, 2               ; skip two spaces
-            mov [esp + 10], bx      ; third word address
-            call get_word_length
+            add bx, [esp + 4]                               ; skip to the third word by adding to base
+            add bx, [esp + 8]                               ; address combined length of two previous words
+            add bx, 2                                       ; skip two spaces
+            mov [esp + 10], bx                              ; third word address
+            call get_word_length                            ; check if third word exists
             cmp ax, 0
             je invalid_input
-            mov [esp + 12], ax      ; third word length
+            mov [esp + 12], ax                              ; third word length
 
             ; display parsed words
             mov dx, [esp + 2]
@@ -71,88 +58,77 @@ segment main
             call print_n
 
             mov dx, string_newline
-            call print
+            call print_string
 
             mov dx, [esp + 6]
             mov cx, [esp + 8]
             call print_n
 
             mov dx, string_newline
-            call print
+            call print_string
 
             mov dx, [esp + 10]
             mov cx, [esp + 12]
             call print_n
 
             mov dx, string_newline
-            call print
+            call print_string
 
-            ; clear the stack and go back to loop
-            add esp, 12
-            jmp main_loop
+            jmp main_loop                                   ; go back to main loop
 
-            ; display message that the input is invalid
-            invalid_input:
-                ; clear the stack
-                add esp, 12
+            invalid_input:                                  ; display message that the input is invalid
+                add esp, 12                                 ; remove those 6 variables from stack
 
-                ; compare input to 'exit' command
                 mov si, word string_input
                 mov di, word string_command_exit
-                call compare
+                call compare_strings                        ; compare input to 'exit' command
                 cmp ax, 0
-                je exit     ; exit the application if requested
+                je finish                                   ; exit the application if requested
 
-                ; display invalid input error and jump back to loop
-                mov dx, word string_invalid_input
-                call print
+                mov dx, word string_invalid_input           ; display invalid input error and jump back to loop
+                call print_string
                 jmp main_loop
 
-        finish:
-            ; exit the application with error code 0
+        finish:                                             ; exit application with error code 0
             mov al, 0
             call exit
 
 
     ; print string passed in DX
-    print:
+    print_string:
         mov bx, dx
-        mov cl, byte [bx]   ; get string lenght
+        mov cl, byte [bx]                                   ; get string lenght
         xor ch, ch
-        add dx, 1           ; set beginning of a string
-        call print_n
+        add dx, 1                                           ; point to beginning of a string
+        call print_n                                        ; delegate to print_n
         ret
 
 
     ; print string using DOS 21h interrupt
-    ; BX - file handle
     ; CX - number of bytes to write
     ; DX - string
     print_n:
-        mov bx, 1   ; stdout
+        mov bx, 1                                           ; file handle - stdout
         mov ah, 40h
         int 21h
         ret
 
 
     ; read from standard input to buffer passed in DX
-    read:
-        mov dx, string_input - 1            ; set string pointer
-        mov cl, byte [string_input - 1]     ; get maximum length of the input
-        mov bx, string_input + 1            ; set beginning address of the input for clearing
+    read_string_input:
+        mov dx, string_input - 1                            ; set string pointer
+        mov cl, byte [string_input - 1]                     ; get maximum length of the input
+        mov bx, string_input + 1                            ; set beginning address of the input for clearing
 
-        ; fill the buffer with zeroes
-        clear_loop:
+        clear_loop:                                         ; fill the string_input data with zeroes
             mov [bx], byte 0
             inc bx
             dec cl
             cmp cl, 0
             ja clear_loop
 
-        ; read string using DOS 21h interrupt
-        ; DX - buffer
-        mov ah, 0ah
-        int 21h
+        mov ah, 0ah                                         ; read string using DOS 21h interrupt
+        int 21h                                             ; DX - buffer
         ret
 
 
@@ -161,21 +137,18 @@ segment main
     ; stores result in AX
     ; 0 - strings are the same
     ; 1 - strings are different
-    compare:
-        mov al, [si]    ; length of first string
-        mov ah, [di]    ; length of second string
+    compare_strings:
+        mov al, [si]                                        ; length of first string
+        mov ah, [di]                                        ; length of second string
 
-        cmp al, ah              ; compare length of both strings
-        jne compare_mismatch    ; if lengths are different strings are also different
+        cmp al, ah                                          ; compare length of both strings
+        jne compare_mismatch                                ; if lengths are different strings are also different
 
-        ; position both pointers on first letter
-        add si, 1
+        add si, 1                                           ; position both pointers on first letter
         add di, 1
 
-        ; keep the length of the string in CL
-        mov cl, al
-
-        jmp compare_n
+        mov cl, al                                          ; keep the length of the string in CL
+        jmp compare_n                                       ; delegate to compare_n
 
 
     ; compare CL bytes in two buffers and check if they are equal
@@ -184,68 +157,56 @@ segment main
     ; 0 - strings are the same
     ; 1 - strings are different
     compare_n:
-        ; compare characters one by one
-        compare_loop:
-            ; get letter from both strings
-            mov al, byte [si]
+        compare_loop:                                       ; loop comparing bytes one by one
+
+            mov al, byte [si]                               ; get letter from both strings
             mov ah, byte [di]
 
-            ; compare those letters
-            cmp al, ah
+            cmp al, ah                                      ; compare those letters
             jne compare_mismatch
 
-            ; move both pointers to next character
-            inc si
+            inc si                                          ; move both pointers to next character
             inc di
 
-            ; decrease loop counter
-            dec cl
+            dec cl                                          ; decrease loop counter
             cmp cl, 0
+            ja compare_loop                                 ; continue if there are more bytes to compare
 
-            ; if not every character has been compared
-            ja compare_loop
-
-        ; strings are equal - return 0
-        compare_match:
+        compare_match:                                      ; return 0 if strings are equal
             mov ax, 0
             ret
 
-        ; strings are different - return 1
-        compare_mismatch:
+        compare_mismatch:                                   ; return 1 if strings are equal
             mov ax, 1
             ret
 
 
-    ; return length of the first word found
+    ; return length of the first word found - delimited by space or CR
     ; and stores it in AX
     ; BX - string
     ; return AX - word length
     get_word_length:
-        mov al, [string_input - 1]  ; maximum string length
-        mov cx, 0                   ; character counter
+        mov al, [string_input - 1]                          ; maximum string length
+        mov cx, 0                                           ; character counter
 
         get_word_length_loop:
-            mov dl, byte [bx]   ; load byte from string
-            cmp dl, ' '         ; check if the word has ended
+            mov dl, byte [bx]                               ; load byte from string
+            cmp dl, ' '                                     ; check if the word has ended
             je get_word_length_space_found
-            cmp dl, 13
+            cmp dl, 13                                      ; CR is also a delimiter
             je get_word_length_space_found
 
-            ; increment the counter and move the pointer
-            ; to the next character
-            inc cl
-            inc bx
+            inc cl                                          ; increase the counter
+            inc bx                                          ; move to next byte
 
-            ; if counter hasn't yet exceeded maximum length
-            ; jump to the beginning of the loop
-            cmp cl, al
-            jb get_word_length_loop
+            cmp cl, al                                      ; if counter hasn't yet exceeded maximum length
+            jb get_word_length_loop                         ; jump to the beginning of the loop
 
-        mov ax, 0
+        mov ax, 0                                           ; return 0 if word hasn't been found
         ret
 
-        get_word_length_space_found:
-            mov ax, cx
+        get_word_length_space_found:                        ; if the space was found
+            mov ax, cx                                      ; return word length
             ret
 
 
@@ -264,11 +225,12 @@ segment text
     ;     byte length;
     ;     byte data[length];
     ; }
-
-    string_welcome          db 33, 'Welcome to simple calculator!', 13, 10, 13, 10
-    string_prompt           db 2, '> '
-    string_newline          db 2, 13, 10
+    string_welcome          db 31, 'Welcome to simple calculator!', 13, 10
+    string_type_exit        db 43, 'Type ', 39, 'exit', 39, ' to terminate the application.', 13, 10
+    string_prompt           db 18, 'Enter expression> '
     string_invalid_input    db 16, 'Invalid input!', 13, 10
+    string_newline          db 2, 13, 10
+
     string_command_exit     db 4, 'exit'
 
     ; reserve 32 bytes for reading user input using DOS interrupt
