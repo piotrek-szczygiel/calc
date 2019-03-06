d7a3fa3e28c7225c842fb03c6e906181baa84804
Piotr Szczygieł <szczygiel.p@gmail.com>
2019-03-07 00:08:01 +0100

    Display the result

--- calc.asm
+++ calc.asm
@@ -10,11 +10,11 @@ start:
     mov ds, ax
 
     mov dx, str_welcome
-    call print
+    call print_crlf
 
     .loop:
         mov dx, str_prompt
-        call print_no_crlf
+        call print
 
         call read_input
 
@@ -34,6 +34,7 @@ start:
         jne .loop
 
         call calculate_result
+        call display_result
         jmp .loop
 
 
@@ -42,6 +43,83 @@ start:
 exit:
     mov ah, 4ch
     int 21h
+
+
+display_result:
+    mov dx, str_result
+    call print
+
+    cmp [result.sign], byte 0
+    je .display
+
+    .negative:
+        mov dx, str_negative_result
+        call print
+
+    .display:
+        mov al, byte [result]
+        cmp al, byte 20
+        jae .big
+
+        .small:
+            mov di, numbers_twenty.start
+            call get_nth
+            mov dx, di
+            call print_crlf
+            ret
+
+        .big:
+            call split_result
+            mov al, byte [result.tens]
+            sub al, byte 2
+            mov di, numbers_tens.start
+            call get_nth
+            mov dx, di
+            call print
+            mov al, byte [result.units]
+            cmp al, 0
+            je .finish
+            mov dx, str_hyphen
+            call print
+            mov di, numbers_ten.start
+            call get_nth
+            mov dx, di
+            call print
+
+    .finish:
+        mov dx, str_crlf
+        call print
+        ret
+
+
+; get N-th string from dollar delimited array
+; pass beginning of a array in DI, and index in AL
+; stores result in DI
+get_nth:
+    mov cl, 0
+
+    .loop:
+        cmp al, cl
+        je .found
+
+        call seek_after
+        inc cl
+        jmp .loop
+
+    .found:
+        ret
+
+
+
+; split resulting number into tens and units
+split_result:
+    mov al, byte [result]
+    xor ah, ah
+    mov bx, 10
+    div bl
+    mov [result.tens], byte al
+    mov [result.units], byte ah
+    ret
 
 
 ; calculates the result of the expression
@@ -104,17 +182,17 @@ validate_parsed_input:
     .invalid_operator:
         mov dx, str_invalid_operator
     .invalid:
-        call print
+        call print_crlf
         cmp sp, bp
         ret
 
 
 ; parse word passed in SI with match table passed in DI
 ; pass address terminating the table in DX
-; result will be stored in CL
+; result will be stored in CX
 parse_word:
     mov bx, si
-    mov cl, 0
+    mov cx, 0
 
     .loop:
         cmp di, dx
@@ -124,39 +202,45 @@ parse_word:
         call str_compare
         je .finish
 
-        .seek:
-            cmp [di], byte '$'
-            jne .not_dollar
-            jmp .dollar
-            .not_dollar:
-                inc di
-                jmp .seek
-            .dollar:
-                inc di
+        call seek_after
 
-        inc cl
+        inc cx
         jmp .loop
 
     .finish:
         ret
 
 
+; seek string pointer specified in DI
+; to the address after the end of it
+seek_after:
+    cmp [di], byte '$'
+    jne .not_dollar
+    jmp .dollar
+    .not_dollar:
+        inc di
+        jmp seek_after
+    .dollar:
+        inc di
+        ret
+
+
 ; parse all three words
 parse_input:
     mov si, words.first
-    mov di, numbers_small
-    mov dx, numbers_small.end
+    mov di, numbers_ten.start
+    mov dx, numbers_ten.end
     call parse_word
     mov [number.first], byte cl
 
     mov si, words.third
-    mov di, numbers_small
-    mov dx, numbers_small.end
+    mov di, numbers_ten.start
+    mov dx, numbers_ten.end
     call parse_word
     mov [number.second], byte cl
 
     mov si, words.second
-    mov di, operators
+    mov di, operators.start
     mov dx, operators.end
     call parse_word
     mov [operator], byte cl
@@ -178,7 +262,7 @@ validate_unparsed_input:
 
     .invalid:
         mov dx, str_invalid_input
-        call print
+        call print_crlf
 
         cmp sp, bp
         ret
@@ -256,7 +340,7 @@ str_compare:
 
 ; print string to stdout, add newline
 ; DX - dollar terminated string
-print:
+print_crlf:
     mov ah, 09h
     int 21h
 
@@ -267,7 +351,7 @@ print:
 
 ; print string to stdout, without adding a newline
 ; DX - dollar terminated string
-print_no_crlf:
+print:
     mov ah, 09h
     int 21h
     ret
@@ -296,7 +380,7 @@ read_input:
     call user_input_to_dollar
 
     mov dx, str_crlf
-    call print_no_crlf
+    call print
     ret
 
 
@@ -318,17 +402,23 @@ user_input_to_dollar:
 
 
 segment text
-str_crlf                    db 13, 10, '$'
-str_welcome                 db 'Simple calculator.', 13, 10, '$'
-str_prompt                  db 'Enter expression: $'
+str_welcome                 db 'Simple ASM calculator', 13, 10
+                            db 'Piotr Szczygiel 2019', 13, 10
+                            db  'Type exit to terminate the program$'
+str_prompt                  db  13, 10, 'Enter expression: $'
+str_result                  db 'Result: $'
 str_invalid_input           db 'Invalid input!$'
 str_invalid_first_number    db 'Invalid first number!$'
 str_invalid_second_number   db 'Invalid second number!$'
 str_invalid_operator        db 'Invalid operator!$'
+str_negative_result         db 'minus $'
+str_hyphen                  db '-$'
+str_crlf                    db 13, 10, '$'
 
 command_exit                db 'exit$'
 
-numbers_small               db 'zero$'
+numbers_twenty.start:
+numbers_ten.start:          db 'zero$'
                             db 'one$'
                             db 'two$'
                             db 'three$'
@@ -338,21 +428,42 @@ numbers_small               db 'zero$'
                             db 'seven$'
                             db 'eight$'
                             db 'nine$'
-numbers_small.end:
 
-operators                   db 'plus$'
+numbers_ten.end:            db 'ten$'
+                            db 'eleven$'
+                            db 'twelve$'
+                            db 'thirteen$'
+                            db 'fourteen$'
+                            db 'fifteen$'
+                            db 'sixteen$'
+                            db 'seventeen$'
+                            db 'eighteen$'
+                            ; 19 is impossible to obtain
+numbers_twenty.end:
+
+numbers_tens.start:         db 'twenty$'
+                            db 'thirty$'
+                            db 'forty$'
+                            db 'fifty$'
+                            db 'sixty$'
+                            db 'seventy$'
+                            db 'eighty$'
+                            ; 81 is maximum result possible to obtain
+numbers_tens.end:
+
+operators.start:            db 'plus$'
                             db 'minus$'
                             db 'times$'
 operators.end:
 
-user_input.dos              db 32
+user_input.dos              db 20
 user_input.len              db 0
-user_input                  rb 32
+user_input                  rb 20
 user_input.end:
 
-words.first                 rb 32
-words.second                rb 32
-words.third                 rb 32
+words.first                 rb 20
+words.second                rb 20
+words.third                 rb 20
 words.end:
 
 number.first                rb 1
@@ -362,3 +473,5 @@ operator                    rb 1
 
 result                      rb 1
 result.sign                 rb 1
+result.tens                 rb 1
+result.units                rb 1
